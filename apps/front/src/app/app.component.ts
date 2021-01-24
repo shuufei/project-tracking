43d8014fe2fc9b4bfc11866c267c5590ba685989
shuufei <digital.shuufei@gmail.com>
2021-01-25 02:16:09 +0900

    selectorのプレフィックスを変更 bison -> bis

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'bison-root',
+  selector: 'bis-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
