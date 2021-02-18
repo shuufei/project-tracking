724b0b7a4b62dd5e6270bcb2e81ed5f1e50d4a29
shuufei <digital.shuufei@gmail.com>
2021-02-18 23:38:05 +0900

    add apollo angular

--- apps/front/src/app/app.component.ts
+++ apps/front/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo, gql } from 'apollo-angular';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'bis-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'front';
+
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .query({
+        query: gql`
+          {
+            sample {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .pipe(tap(console.log))
+      .subscribe();
+  }
 }
