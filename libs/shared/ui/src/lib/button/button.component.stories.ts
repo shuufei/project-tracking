55021238fd3a63196475026d21d87bd25a0478b3
shuufei <digital.shuufei@gmail.com>
2021-02-20 11:52:35 +0900

    add disabled button style

--- libs/shared/ui/src/lib/button/button.component.stories.ts
+++ libs/shared/ui/src/lib/button/button.component.stories.ts
@@ -20,6 +20,18 @@ export const Default = () => ({
   props: {},
 });
 
+export const Default_Disabled = () => ({
+  moduleMetadata,
+  template: `
+    <button ui-button class="m-4" disabled>Disabled</button>
+    <button ui-button color="basic" class="m-4" disabled>Basic</button>
+    <button ui-button color="primary" class="m-4" disabled>Primary</button>
+    <button ui-button color="success" class="m-4" disabled>Success</button>
+    <button ui-button color="warn" class="m-4" disabled>Warn</button>
+  `,
+  props: {},
+});
+
 export const Stroked = () => ({
   moduleMetadata,
   template: `
@@ -32,6 +44,18 @@ export const Stroked = () => ({
   props: {},
 });
 
+export const Stroked_Disabled = () => ({
+  moduleMetadata,
+  template: `
+    <button ui-stroked-button class="m-4" disabled>Default</button>
+    <button ui-stroked-button color="basic" class="m-4" disabled>Basic</button>
+    <button ui-stroked-button color="primary" class="m-4" disabled>Primary</button>
+    <button ui-stroked-button color="success" class="m-4" disabled>Success</button>
+    <button ui-stroked-button color="warn" class="m-4" disabled>Warn</button>
+  `,
+  props: {},
+});
+
 export const Fill = () => ({
   moduleMetadata,
   template: `
@@ -40,6 +64,18 @@ export const Fill = () => ({
     <button ui-fill-button color="primary" class="m-4">Primary</button>
     <button ui-fill-button color="success" class="m-4">Success</button>
     <button ui-fill-button color="warn" class="m-4">Warn</button>
+  `,
+  props: {},
+});
+
+export const Fill_Disabled = () => ({
+  moduleMetadata,
+  template: `
+    <button ui-fill-button class="m-4" disabled>Default</button>
+    <button ui-fill-button color="basic" class="m-4" disabled>Basic</button>
+    <button ui-fill-button color="primary" class="m-4" disabled>Primary</button>
+    <button ui-fill-button color="success" class="m-4" disabled>Success</button>
+    <button ui-fill-button color="warn" class="m-4" disabled>Warn</button>
   `,
   props: {},
 });
