315699d10eb3eda273b982954869e9bad513f2ef
shuufei <digital.shuufei@gmail.com>
2021-02-23 02:24:55 +0900

    global-styleファイルを削除

--- libs/shared/ui/src/global-styles.ts
+++
@@ -1,3 +0,0 @@
-import '!style-loader!css-loader!less-loader!../../../../node_modules/@taiga-ui/core/styles/taiga-ui-global.less';
-import '!style-loader!css-loader!less-loader!../../../../node_modules/@taiga-ui/core/styles/taiga-ui-theme.less';
-import '!style-loader!css-loader!sass-loader!./styles/index.scss';
