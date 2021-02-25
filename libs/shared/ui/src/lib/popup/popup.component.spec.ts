f8729e00fa52c54cffc4036a82af121cabc2c42d
shuufei <digital.shuufei@gmail.com>
2021-02-25 23:53:58 +0900

    popup componentを追加

---
+++ libs/shared/ui/src/lib/popup/popup.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PopupComponent } from './popup.component';
+import { PopupModule } from './popup.module';
+
+describe('PopupComponent', () => {
+  let component: PopupComponent;
+  let fixture: ComponentFixture<PopupComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PopupModule],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PopupComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
