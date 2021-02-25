4fdd2394ef71ce8c707403442481f9f824960c99
shuufei <digital.shuufei@gmail.com>
2021-02-25 10:04:25 +0900

    navigation component first commit

---
+++ libs/shared/ui/src/lib/navigation/navigation.component.stories.ts
@@ -0,0 +1,15 @@
+
+import { NavigationComponent } from './navigation.component';
+
+export default {
+  title: 'NavigationComponent'
+}
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: []
+  },
+  component: NavigationComponent,
+  props: {
+  }
+})
--- libs/shared/ui/src/lib/shared-ui.module.ts
+++ libs/shared/ui/src/lib/shared-ui.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ButtonModule } from './button/button.module';
+import { NavigationModule } from './navigation/navigation.module';
 
 @NgModule({
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, NavigationModule],
 })
-export class SharedUiModule {}
+export class SharedUiModule { }
