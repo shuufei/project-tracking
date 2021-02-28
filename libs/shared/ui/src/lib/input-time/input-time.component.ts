fad2aef26a9d145bdfa4ef27b151be3aa07b1a6c
shuufei <digital.shuufei@gmail.com>
2021-02-28 12:44:46 +0900

    input time componentで秒対応

--- libs/shared/ui/src/lib/input-time/input-time.component.ts
+++ libs/shared/ui/src/lib/input-time/input-time.component.ts
@@ -10,7 +10,6 @@ import { FormControl, FormGroup } from '@ngneat/reactive-forms';
 import { RxState } from '@rx-angular/state';
 import { Subject } from 'rxjs';
 import { filter, tap } from 'rxjs/operators';
-// import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'ui-input-time',
@@ -23,39 +22,52 @@ export class InputTimeComponent {
   @Input()
   set hours(value: number) {
     this.time.controls.hours.setValue(
-      value > 0 && value < 10 ? `0${value}` : value,
+      this.isOneDigit(value) ? `0${value}` : value,
       { emitEvent: false }
     );
   }
   @Input()
   set minutes(value: number) {
     this.time.controls.minutes.setValue(
-      value > 0 && value < 10 ? `0${value}` : value,
+      this.isOneDigit(value) ? `0${value}` : value,
       { emitEvent: false }
+    );
+  }
+  @Input()
+  set seconds(value: number) {
+    this.time.controls.seconds.setValue(
+      this.isOneDigit(value) ? `0${value}` : value,
+      {
+        emitEvent: false,
+      }
     );
   }
   @ViewChild('hoursInput', { static: true }) hoursInput: ElementRef;
   @ViewChild('minutesInput', { static: true }) minutesInput: ElementRef;
+  @ViewChild('secondsInput', { static: true }) secondsInput: ElementRef;
   @ViewChild('resetButton', { static: true, read: ElementRef })
   resetButton: ElementRef;
 
   readonly time = new FormGroup<{
     hours: number | string;
     minutes: number | string;
+    seconds: number | string;
   }>({
     hours: new FormControl('00'),
     minutes: new FormControl('00'),
+    seconds: new FormControl('00'),
   });
 
   readonly inputedHoursValue$ = new Subject<InputEvent>();
   readonly inputedMinutesValue$ = new Subject<InputEvent>();
+  readonly inputedSecondsValue$ = new Subject<InputEvent>();
   readonly resetTime$ = new Subject<void>();
   private readonly onClickedHostEl$ = new Subject<Event>();
 
-  private readonly controlHoursValueHandler$ = this.inputedHoursValue$.pipe(
+  private readonly formatHoursValueHandler$ = this.inputedHoursValue$.pipe(
     tap((event) => {
       const currentValue = this.time.value.hours;
-      if (currentValue >= 0 && currentValue <= 9) {
+      if (this.isOneDigit(currentValue)) {
         this.time.controls.hours.setValue(`0${currentValue}`, {
           emitEvent: false,
         });
@@ -75,10 +87,10 @@ export class InputTimeComponent {
     })
   );
 
-  private readonly controlMinutesValueHandler$ = this.inputedMinutesValue$.pipe(
+  private readonly formatMinutesValueHandler$ = this.inputedMinutesValue$.pipe(
     tap((event) => {
       const currentValue = this.time.value.minutes;
-      if (currentValue >= 0 && currentValue <= 9) {
+      if (this.isOneDigit(currentValue)) {
         this.time.controls.minutes.setValue(`0${currentValue}`, {
           emitEvent: false,
         });
@@ -98,11 +110,35 @@ export class InputTimeComponent {
     })
   );
 
+  private readonly formatSecondsValueHandler$ = this.inputedSecondsValue$.pipe(
+    tap((event) => {
+      const currentValue = this.time.value.seconds;
+      if (this.isOneDigit(currentValue)) {
+        this.time.controls.seconds.setValue(`0${currentValue}`, {
+          emitEvent: false,
+        });
+      } else if (currentValue > 60) {
+        this.time.controls.seconds.setValue(`0${event.data}`, {
+          emitEvent: false,
+        });
+      } else if (currentValue < 0) {
+        this.time.controls.seconds.setValue('00', {
+          emitEvent: false,
+        });
+      } else {
+        this.time.controls.seconds.setValue(currentValue, {
+          emitEvent: false,
+        });
+      }
+    })
+  );
+
   private readonly resetTimeHandler$ = this.resetTime$.pipe(
     tap(() => {
       this.time.setValue({
         hours: '00',
         minutes: '00',
+        seconds: '00',
       });
     })
   );
@@ -115,6 +151,9 @@ export class InputTimeComponent {
         ) &&
         !(this.resetButton.nativeElement as HTMLElement).contains(
           event.target as Node
+        ) &&
+        !(this.secondsInput.nativeElement as HTMLElement).contains(
+          event.target as Node
         )
     ),
     tap(() => {
@@ -123,8 +162,8 @@ export class InputTimeComponent {
   );
 
   constructor(private state: RxState<{ [key: string]: never }>) {
-    this.state.hold(this.controlHoursValueHandler$);
-    this.state.hold(this.controlMinutesValueHandler$);
+    this.state.hold(this.formatHoursValueHandler$);
+    this.state.hold(this.formatMinutesValueHandler$);
     this.state.hold(this.resetTimeHandler$);
     this.state.hold(this.focusHoursInputHandler$);
   }
@@ -132,5 +171,9 @@ export class InputTimeComponent {
   @HostListener('click', ['$event'])
   onClick(event: Event) {
     this.onClickedHostEl$.next(event);
+  }
+
+  private isOneDigit(value) {
+    return value > 0 && value < 10;
   }
 }
