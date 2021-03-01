52dec00fd7eca7371b6d2fbfa4b97688d9eb5d18
shuufei <digital.shuufei@gmail.com>
2021-03-01 09:21:48 +0900

    icon componentのstoryを改善

--- libs/shared/ui/src/lib/icon/icon.component.stories.ts
+++ libs/shared/ui/src/lib/icon/icon.component.stories.ts
@@ -1,237 +1,110 @@
+import { Icons } from './icon.component';
 import { IconModule } from './icon.module';
 
 export default {
   title: 'IconComponent',
 };
 
+const defaultSTemplate = Icons.map(
+  (icon) => `<ui-icon size="s" icon="${icon}"></ui-icon>`
+).join('');
 export const default_s = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="s" icon="arrow-down"></ui-icon>
-    <ui-icon size="s" icon="arrow-up"></ui-icon>
-    <ui-icon size="s" icon="arrow-left"></ui-icon>
-    <ui-icon size="s" icon="arrow-right"></ui-icon>
-    <ui-icon size="s" icon="chevron-down"></ui-icon>
-    <ui-icon size="s" icon="chevron-up"></ui-icon>
-    <ui-icon size="s" icon="chevron-left"></ui-icon>
-    <ui-icon size="s" icon="chevron-right"></ui-icon>
-    <ui-icon size="s" icon="chevrons-down"></ui-icon>
-    <ui-icon size="s" icon="chevrons-up"></ui-icon>
-    <ui-icon size="s" icon="chevrons-left"></ui-icon>
-    <ui-icon size="s" icon="chevrons-right"></ui-icon>
-    <ui-icon size="s" icon="columns"></ui-icon>
-    <ui-icon size="s" icon="more-horizontal"></ui-icon>
-    <ui-icon size="s" icon="more-vertical"></ui-icon>
-    <ui-icon size="s" icon="pause"></ui-icon>
-    <ui-icon size="s" icon="play"></ui-icon>
-    <ui-icon size="s" icon="user"></ui-icon>
-    <ui-icon size="s" icon="calendar"></ui-icon>
-    <ui-icon size="s" icon="check"></ui-icon>
+    ${defaultSTemplate}
   `,
   props: {},
 });
 
+const defaultMTemplate = Icons.map(
+  (icon) => `<ui-icon size="m" icon="${icon}"></ui-icon>`
+).join('');
 export const default_m = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="m" icon="arrow-down"></ui-icon>
-    <ui-icon size="m" icon="arrow-up"></ui-icon>
-    <ui-icon size="m" icon="arrow-left"></ui-icon>
-    <ui-icon size="m" icon="arrow-right"></ui-icon>
-    <ui-icon size="m" icon="chevron-down"></ui-icon>
-    <ui-icon size="m" icon="chevron-up"></ui-icon>
-    <ui-icon size="m" icon="chevron-left"></ui-icon>
-    <ui-icon size="m" icon="chevron-right"></ui-icon>
-    <ui-icon size="m" icon="chevrons-down"></ui-icon>
-    <ui-icon size="m" icon="chevrons-up"></ui-icon>
-    <ui-icon size="m" icon="chevrons-left"></ui-icon>
-    <ui-icon size="m" icon="chevrons-right"></ui-icon>
-    <ui-icon size="m" icon="columns"></ui-icon>
-    <ui-icon size="m" icon="more-horizontal"></ui-icon>
-    <ui-icon size="m" icon="more-vertical"></ui-icon>
-    <ui-icon size="m" icon="pause"></ui-icon>
-    <ui-icon size="m" icon="play"></ui-icon>
-    <ui-icon size="m" icon="user"></ui-icon>
-    <ui-icon size="m" icon="calendar"></ui-icon>
-    <ui-icon size="m" icon="check"></ui-icon>
+    ${defaultMTemplate}
   `,
   props: {},
 });
 
+const primarySTemplate = Icons.map(
+  (icon) => `<ui-icon size="s" color="primary" icon="${icon}"></ui-icon>`
+).join('');
 export const primary_s = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="s" color="primary" icon="arrow-down"></ui-icon>
-    <ui-icon size="s" color="primary" icon="arrow-up"></ui-icon>
-    <ui-icon size="s" color="primary" icon="arrow-left"></ui-icon>
-    <ui-icon size="s" color="primary" icon="arrow-right"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevron-down"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevron-up"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevron-left"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevron-right"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevrons-down"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevrons-up"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevrons-left"></ui-icon>
-    <ui-icon size="s" color="primary" icon="chevrons-right"></ui-icon>
-    <ui-icon size="s" color="primary" icon="columns"></ui-icon>
-    <ui-icon size="s" color="primary" icon="more-horizontal"></ui-icon>
-    <ui-icon size="s" color="primary" icon="more-vertical"></ui-icon>
-    <ui-icon size="s" color="primary" icon="pause"></ui-icon>
-    <ui-icon size="s" color="primary" icon="play"></ui-icon>
-    <ui-icon size="s" color="primary" icon="user"></ui-icon>
-    <ui-icon size="s" color="primary" icon="calendar"></ui-icon>
-    <ui-icon size="s" color="check" icon="check"></ui-icon>
+    ${primarySTemplate}
   `,
   props: {},
 });
 
+const primaryMTemplate = Icons.map(
+  (icon) => `<ui-icon size="m" color="primary" icon="${icon}"></ui-icon>`
+).join('');
 export const primary_m = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="m" color="primary" icon="arrow-down"></ui-icon>
-    <ui-icon size="m" color="primary" icon="arrow-up"></ui-icon>
-    <ui-icon size="m" color="primary" icon="arrow-left"></ui-icon>
-    <ui-icon size="m" color="primary" icon="arrow-right"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevron-down"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevron-up"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevron-left"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevron-right"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevrons-down"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevrons-up"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevrons-left"></ui-icon>
-    <ui-icon size="m" color="primary" icon="chevrons-right"></ui-icon>
-    <ui-icon size="m" color="primary" icon="columns"></ui-icon>
-    <ui-icon size="m" color="primary" icon="more-horizontal"></ui-icon>
-    <ui-icon size="m" color="primary" icon="more-vertical"></ui-icon>
-    <ui-icon size="m" color="primary" icon="pause"></ui-icon>
-    <ui-icon size="m" color="primary" icon="play"></ui-icon>
-    <ui-icon size="m" color="primary" icon="user"></ui-icon>
-    <ui-icon size="m" color="primary" icon="calendar"></ui-icon>
-    <ui-icon size="m" color="primary" icon="check"></ui-icon>
+    ${primaryMTemplate}
   `,
   props: {},
 });
 
+const successSTemplate = Icons.map(
+  (icon) => `<ui-icon size="s" color="success" icon="${icon}"></ui-icon>`
+).join('');
 export const success_s = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="s" color="success" icon="arrow-down"></ui-icon>
-    <ui-icon size="s" color="success" icon="arrow-up"></ui-icon>
-    <ui-icon size="s" color="success" icon="arrow-left"></ui-icon>
-    <ui-icon size="s" color="success" icon="arrow-right"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevron-down"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevron-up"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevron-left"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevron-right"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevrons-down"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevrons-up"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevrons-left"></ui-icon>
-    <ui-icon size="s" color="success" icon="chevrons-right"></ui-icon>
-    <ui-icon size="s" color="success" icon="columns"></ui-icon>
-    <ui-icon size="s" color="success" icon="more-horizontal"></ui-icon>
-    <ui-icon size="s" color="success" icon="more-vertical"></ui-icon>
-    <ui-icon size="s" color="success" icon="pause"></ui-icon>
-    <ui-icon size="s" color="success" icon="play"></ui-icon>
-    <ui-icon size="s" color="success" icon="user"></ui-icon>
-    <ui-icon size="s" color="success" icon="calendar"></ui-icon>
-    <ui-icon size="s" color="success" icon="check"></ui-icon>
+    ${successSTemplate}
   `,
   props: {},
 });
 
+const successMTemplate = Icons.map(
+  (icon) => `<ui-icon size="m" color="success" icon="${icon}"></ui-icon>`
+).join('');
 export const success_m = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="m" color="success" icon="arrow-down"></ui-icon>
-    <ui-icon size="m" color="success" icon="arrow-up"></ui-icon>
-    <ui-icon size="m" color="success" icon="arrow-left"></ui-icon>
-    <ui-icon size="m" color="success" icon="arrow-right"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevron-down"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevron-up"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevron-left"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevron-right"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevrons-down"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevrons-up"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevrons-left"></ui-icon>
-    <ui-icon size="m" color="success" icon="chevrons-right"></ui-icon>
-    <ui-icon size="m" color="success" icon="columns"></ui-icon>
-    <ui-icon size="m" color="success" icon="more-horizontal"></ui-icon>
-    <ui-icon size="m" color="success" icon="more-vertical"></ui-icon>
-    <ui-icon size="m" color="success" icon="pause"></ui-icon>
-    <ui-icon size="m" color="success" icon="play"></ui-icon>
-    <ui-icon size="m" color="success" icon="user"></ui-icon>
-    <ui-icon size="m" color="success" icon="calendar"></ui-icon>
-    <ui-icon size="m" color="success" icon="check"></ui-icon>
+    ${successMTemplate}
   `,
   props: {},
 });
 
+const warnSTemplate = Icons.map(
+  (icon) => `<ui-icon size="s" color="warn" icon="${icon}"></ui-icon>`
+).join('');
 export const warn_s = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="s" color="warn" icon="arrow-down"></ui-icon>
-    <ui-icon size="s" color="warn" icon="arrow-up"></ui-icon>
-    <ui-icon size="s" color="warn" icon="arrow-left"></ui-icon>
-    <ui-icon size="s" color="warn" icon="arrow-right"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevron-down"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevron-up"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevron-left"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevron-right"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevrons-down"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevrons-up"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevrons-left"></ui-icon>
-    <ui-icon size="s" color="warn" icon="chevrons-right"></ui-icon>
-    <ui-icon size="s" color="warn" icon="columns"></ui-icon>
-    <ui-icon size="s" color="warn" icon="more-horizontal"></ui-icon>
-    <ui-icon size="s" color="warn" icon="more-vertical"></ui-icon>
-    <ui-icon size="s" color="warn" icon="pause"></ui-icon>
-    <ui-icon size="s" color="warn" icon="play"></ui-icon>
-    <ui-icon size="s" color="warn" icon="user"></ui-icon>
-    <ui-icon size="s" color="warn" icon="calendar"></ui-icon>
-    <ui-icon size="s" color="warn" icon="check"></ui-icon>
+    ${warnSTemplate}
   `,
   props: {},
 });
 
+const warnMTemplate = Icons.map(
+  (icon) => `<ui-icon size="m" color="warn" icon="${icon}"></ui-icon>`
+).join('');
 export const warn_m = () => ({
   moduleMetadata: {
     imports: [IconModule],
   },
   template: `
-    <ui-icon size="m" color="warn" icon="arrow-down"></ui-icon>
-    <ui-icon size="m" color="warn" icon="arrow-up"></ui-icon>
-    <ui-icon size="m" color="warn" icon="arrow-left"></ui-icon>
-    <ui-icon size="m" color="warn" icon="arrow-right"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevron-down"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevron-up"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevron-left"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevron-right"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevrons-down"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevrons-up"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevrons-left"></ui-icon>
-    <ui-icon size="m" color="warn" icon="chevrons-right"></ui-icon>
-    <ui-icon size="m" color="warn" icon="columns"></ui-icon>
-    <ui-icon size="m" color="warn" icon="more-horizontal"></ui-icon>
-    <ui-icon size="m" color="warn" icon="more-vertical"></ui-icon>
-    <ui-icon size="m" color="warn" icon="pause"></ui-icon>
-    <ui-icon size="m" color="warn" icon="play"></ui-icon>
-    <ui-icon size="m" color="warn" icon="user"></ui-icon>
-    <ui-icon size="m" color="warn" icon="calendar"></ui-icon>
-    <ui-icon size="m" color="warn" icon="check"></ui-icon>
+    ${warnMTemplate}
   `,
   props: {},
 });
