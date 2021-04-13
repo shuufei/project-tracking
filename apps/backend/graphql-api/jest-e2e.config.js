559013b866f9f67c187c1c6e940ec84f78b040c2
shuufei <digital.shuufei@gmail.com>
2021-04-13 23:14:14 +0900

    e2eのサンプルコードを追加

---
+++ apps/backend/graphql-api/jest-e2e.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'backend-graphql-api-e2e',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.e2e.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/backend/graphql-api-e2e',
+};
