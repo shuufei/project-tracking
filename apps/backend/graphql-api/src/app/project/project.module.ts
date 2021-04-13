473500ee58045cf02a4bf93373f0d93dd5693a07
shuufei <digital.shuufei@gmail.com>
2021-04-13 23:05:03 +0900

    mockを整理

--- apps/backend/graphql-api/src/app/project/project.module.ts
+++ apps/backend/graphql-api/src/app/project/project.module.ts
@@ -1,139 +1,19 @@
 import {
   GET_BACKLOG_BY_PROJECT_ID_SERVICE,
-  IGetBacklogByProjectIdService,
-  IListBoardsByProjectIdService,
-  IListUsersByProjectIdService,
   ListProjectsService,
+  LIST_BOARDS_BY_PROJECT_ID_SERVICE,
   LIST_PROJECTS_SERVICE,
   LIST_USERS_BY_PROJECT_ID_SERVICE,
 } from '@bison/backend/application';
-import {
-  IProjectRepository,
-  ListResponse,
-  PROJECT_REPOSITORY,
-} from '@bison/backend/domain';
-import type { Color } from '@bison/shared/domain';
+import { PROJECT_REPOSITORY } from '@bison/backend/domain';
 import { Module } from '@nestjs/common';
-import { LIST_BOARDS_BY_PROJECT_ID_SERVICE } from '../../../../../../libs/backend/application/src/lib/board/interface/list-boards-by-project-id-service';
-import { getRandom } from '../util/get-random-number';
 import { ProjectResolver } from './project.resolver';
-
-class MockProjectRepository implements IProjectRepository {
-  async list(): Promise<ListResponse> {
-    return {
-      edges: [
-        {
-          cursor: '',
-          node: {
-            id: `project${getRandom()}`,
-            name: `project name ${getRandom()}`,
-            description: `project description ${getRandom()}`,
-            color: 'red' as Color,
-          },
-        },
-        {
-          cursor: '',
-          node: {
-            id: `project${getRandom()}`,
-            name: `project name ${getRandom()}`,
-            description: `project description ${getRandom()}`,
-            color: 'blue' as Color,
-          },
-        },
-        {
-          cursor: '',
-          node: {
-            id: `project${getRandom()}`,
-            name: `project name ${getRandom()}`,
-            description: `project description ${getRandom()}`,
-            color: 'green' as Color,
-          },
-        },
-      ],
-    };
-  }
-}
-
-class MockGetBacklogByProjectIdService
-  implements IGetBacklogByProjectIdService {
-  handle() {
-    return Promise.resolve({
-      id: `mock backlog id ${getRandom()}`,
-    });
-  }
-}
-
-class MockListBoardsByProjectIdService
-  implements IListBoardsByProjectIdService {
-  async handle(): ReturnType<IListBoardsByProjectIdService['handle']> {
-    return {
-      edges: [
-        {
-          cursor: '',
-          node: {
-            id: `board${getRandom()}`,
-            name: `board name ${getRandom()}`,
-            description: `board description ${getRandom()}`,
-            isArchived: false,
-          },
-        },
-        {
-          cursor: '',
-          node: {
-            id: `board${getRandom()}`,
-            name: `board name ${getRandom()}`,
-            description: `board description ${getRandom()}`,
-            isArchived: false,
-          },
-        },
-        {
-          cursor: '',
-          node: {
-            id: `board${getRandom()}`,
-            name: `board name ${getRandom()}`,
-            description: `board description ${getRandom()}`,
-            isArchived: false,
-          },
-        },
-      ],
-      hasNextPage: false,
-    };
-  }
-}
-
-class MockListUsersByProjectIdService implements IListUsersByProjectIdService {
-  async handle(): ReturnType<IListUsersByProjectIdService['handle']> {
-    return {
-      edges: [
-        {
-          cursor: '',
-          node: {
-            id: `user${getRandom()}`,
-            name: `user name ${getRandom()}`,
-            icon: `user description ${getRandom()}`,
-          },
-        },
-        {
-          cursor: '',
-          node: {
-            id: `user${getRandom()}`,
-            name: `user name ${getRandom()}`,
-            icon: `user icon ${getRandom()}`,
-          },
-        },
-        {
-          cursor: '',
-          node: {
-            id: `user${getRandom()}`,
-            name: `user name ${getRandom()}`,
-            icon: `user icon ${getRandom()}`,
-          },
-        },
-      ],
-      hasNextPage: false,
-    };
-  }
-}
+import {
+  MockGetBacklogByProjectIdService,
+  MockListBoardsByProjectIdService,
+  MockListUsersByProjectIdService,
+  MockProjectRepository,
+} from './testing/mock';
 
 @Module({
   providers: [
---
+++ apps/backend/graphql-api/src/app/project/testing/mock.ts
@@ -0,0 +1,147 @@
+import {
+  GetBacklogByProjectIdResponse,
+  IGetBacklogByProjectIdService,
+  IListBoardsByProjectIdService,
+  IListProjectsService,
+  IListUsersByProjectIdService,
+  ListBoardsByProjectIdResponse,
+  ListProjectsResponse,
+  ListUsersByProjectIdResponse,
+} from '@bison/backend/application';
+import { IProjectRepository } from '@bison/backend/domain';
+import { Color } from '@bison/shared/domain';
+
+export const listProjectsResponse: ListProjectsResponse = {
+  hasNextPage: true,
+  edges: [
+    {
+      cursor: 'project_cursor_0001',
+      node: {
+        id: `project0001`,
+        name: `project name 0001`,
+        description: `project description 0001`,
+        color: 'red' as Color,
+      },
+    },
+    {
+      cursor: 'project_cursor_0002',
+      node: {
+        id: `project0002`,
+        name: `project name 0002`,
+        description: `project description 0002`,
+        color: 'blue' as Color,
+      },
+    },
+    {
+      cursor: 'project_cursor_0003',
+      node: {
+        id: `project0003`,
+        name: `project name 0003`,
+        description: `project description 0003`,
+        color: 'green' as Color,
+      },
+    },
+  ],
+};
+
+export const getBacklogByProjectIdResponse: GetBacklogByProjectIdResponse = {
+  id: `mock backlog id 0001`,
+};
+
+export const listBoardsByProjectIdResponse: ListBoardsByProjectIdResponse = {
+  edges: [
+    {
+      cursor: 'board_cursor_0001',
+      node: {
+        id: `board0001`,
+        name: `board name 0001`,
+        description: `board description 0001`,
+        isArchived: false,
+      },
+    },
+    {
+      cursor: 'board_cursor_0002',
+      node: {
+        id: `board0002`,
+        name: `board name 0002`,
+        description: `board description 0002`,
+        isArchived: false,
+      },
+    },
+    {
+      cursor: 'board_cursor_0003',
+      node: {
+        id: `board0003`,
+        name: `board name 0003`,
+        description: `board description 0003`,
+        isArchived: false,
+      },
+    },
+  ],
+  hasNextPage: false,
+};
+
+export const listUsersByProjectIdResponse: ListUsersByProjectIdResponse = {
+  edges: [
+    {
+      cursor: 'user_cursor_0001',
+      node: {
+        id: `user0001`,
+        name: `user name 0001`,
+        icon: `user description 0001`,
+      },
+    },
+    {
+      cursor: 'user_cursor_0002',
+      node: {
+        id: `user0002`,
+        name: `user name 0002`,
+        icon: `user icon 0002`,
+      },
+    },
+    {
+      cursor: 'user_cursor_0003',
+      node: {
+        id: `user0003`,
+        name: `user name 0003`,
+        icon: `user icon 0003`,
+      },
+    },
+  ],
+  hasNextPage: false,
+};
+
+export class MockProjectRepository implements IProjectRepository {
+  async list() {
+    return {
+      edges: listProjectsResponse.edges,
+    };
+  }
+}
+
+export class MockListProjectsService implements IListProjectsService {
+  async handle() {
+    return listProjectsResponse;
+  }
+}
+
+export class MockGetBacklogByProjectIdService
+  implements IGetBacklogByProjectIdService {
+  async handle() {
+    return getBacklogByProjectIdResponse;
+  }
+}
+
+export class MockListBoardsByProjectIdService
+  implements IListBoardsByProjectIdService {
+  async handle() {
+    return listBoardsByProjectIdResponse;
+  }
+}
+
+export class MockListUsersByProjectIdService
+  implements IListUsersByProjectIdService {
+  async handle() {
+    return listUsersByProjectIdResponse;
+  }
+}
