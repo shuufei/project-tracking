4bb729650ff78b6dde0787a0222a6f3e78e94b04
shuufei <digital.shuufei@gmail.com>
2021-05-13 00:02:20 +0900

    schema定義修正

--- libs/shared/schema/src/lib/schema-types.ts
+++ libs/shared/schema/src/lib/schema-types.ts
@@ -6,6 +6,11 @@
 
 /* tslint:disable */
 /* eslint-disable */
+export enum BoardTaskType {
+    TASKGROUP = "TASKGROUP",
+    TASK = "TASK"
+}
+
 export enum Color {
     RED = "RED",
     BLUE = "BLUE",
@@ -16,12 +21,18 @@ export enum Color {
     GRAY = "GRAY"
 }
 
+export enum Status {
+    TODO = "TODO",
+    INPROGRESS = "INPROGRESS",
+    CONFIRM = "CONFIRM",
+    DONE = "DONE"
+}
+
 export interface CreateProjectInput {
     name: string;
     description?: string;
     color: Color;
     adminUserId: string;
-    memberUserIds: string[];
 }
 
 export interface UpdateProjectInput {
@@ -30,7 +41,133 @@ export interface UpdateProjectInput {
     description?: string;
     color: Color;
     adminUserId: string;
-    memberUserIds: string[];
+}
+
+export interface UpdateProjectMembersInput {
+    addUserIds: string[];
+    removeUserIds: string[];
+}
+
+export interface DeleteProjectInput {
+    id: string;
+}
+
+export interface CreateBoardInput {
+    name: string;
+    description?: string;
+    projectId: string;
+}
+
+export interface UpdateBoardInput {
+    id: string;
+    name: string;
+    description?: string;
+    projectId: string;
+}
+
+export interface DeleteBoardInput {
+    id: string;
+}
+
+export interface CreateTaskOnBoardInput {
+    title: string;
+    description?: string;
+    assignUserId: string;
+    boardId: string;
+    scheduledTime?: number;
+}
+
+export interface CreateTaskOnTaskGroupInput {
+    title: string;
+    description?: string;
+    assignUserId: string;
+    taskGroupId: string;
+    scheduledTime?: number;
+}
+
+export interface UpdateTaskInput {
+    id: string;
+    title: string;
+    description?: string;
+    status: Status;
+    assignUserId: string;
+    workTime?: number;
+    scheduledTime?: number;
+    boardId: string;
+}
+
+export interface DeleteTaskInput {
+    id: string;
+}
+
+export interface CreateTaskGroupInput {
+    title: string;
+    description?: string;
+    assignUserId: string;
+    boardId: string;
+    scheduledTime?: number;
+}
+
+export interface UpdateTaskGroupInput {
+    id: string;
+    title: string;
+    description?: string;
+    status: Status;
+    assignUserId: string;
+    scheduledTime?: number;
+    boardId: string;
+}
+
+export interface DeleteTaskGroupInput {
+    id: string;
+}
+
+export interface CreateSubtaskInput {
+    title: string;
+    description?: string;
+    assignUserId: string;
+    taskId: string;
+    scheduledTime?: number;
+}
+
+export interface UpdateSubtaskInput {
+    id: string;
+    title: string;
+    description?: string;
+    isCompleted: boolean;
+    assignUserId: string;
+    workTime?: number;
+    scheduledTime?: number;
+}
+
+export interface DeleteSubtaskInput {
+    id: string;
+}
+
+export interface BoardTasksOrderItemInput {
+    taskId: string;
+    type: BoardTaskType;
+}
+
+export interface ChangeBoardTasksOrderInput {
+    boardId: string;
+    tasksOrder: BoardTasksOrderItemInput[];
+}
+
+export interface ChangeTasksOrderOfTaskGroupInput {
+    taskGroupId: string;
+    tasksOrder: string[];
+}
+
+export interface ChangeSubtasksOrderOfTaskInput {
+    taskId: string;
+    subtasksOrder: string[];
+}
+
+export interface UpdateMeInput {
+    id: string;
+    name: string;
+    icon?: string;
 }
 
 export interface Node {
@@ -39,10 +176,33 @@ export interface Node {
 
 export interface IQuery {
     viewer(): User | Promise<User>;
+    project(id: string): Project | Promise<Project>;
+    board(id: string): Board | Promise<Board>;
+    users(): User[] | Promise<User[]>;
 }
 
 export interface IMutation {
     createProject(input: CreateProjectInput): Project | Promise<Project>;
+    updateProject(input: UpdateProjectInput): Project | Promise<Project>;
+    deleteProject(input: DeleteProjectInput): Project | Promise<Project>;
+    updateProjectMembers(input: UpdateProjectMembersInput): Project | Promise<Project>;
+    createBoard(input: CreateBoardInput): Project | Promise<Project>;
+    updateBoard(input: UpdateBoardInput): Project | Promise<Project>;
+    deleteBoard(input: DeleteBoardInput): Project | Promise<Project>;
+    createTaskOnBoard(input: CreateTaskOnBoardInput): Task | Promise<Task>;
+    createTaskOnTaskGroup(input: CreateTaskOnTaskGroupInput): Task | Promise<Task>;
+    updateTask(input: UpdateTaskInput): Task | Promise<Task>;
+    deleteTask(input: DeleteTaskInput): Task | Promise<Task>;
+    createTaskGroup(input: CreateTaskGroupInput): TaskGroup | Promise<TaskGroup>;
+    updateTaskGroup(input: UpdateTaskGroupInput): TaskGroup | Promise<TaskGroup>;
+    deleteTaskGroup(input: DeleteTaskGroupInput): TaskGroup | Promise<TaskGroup>;
+    createSubtask(input: CreateSubtaskInput): Subtask | Promise<Subtask>;
+    updateSubtask(input: UpdateSubtaskInput): Subtask | Promise<Subtask>;
+    deleteSubtask(input: DeleteSubtaskInput): Subtask | Promise<Subtask>;
+    changeBoardTasksOrder(input: ChangeBoardTasksOrderInput): Board | Promise<Board>;
+    changeTasksOrderOfTaskGroup(input: ChangeTasksOrderOfTaskGroupInput): TaskGroup | Promise<TaskGroup>;
+    changeSubtasksOrderOfTask(input: ChangeSubtasksOrderOfTaskInput): Task | Promise<Task>;
+    updateMe(input: UpdateMeInput): User | Promise<User>;
 }
 
 export interface User extends Node {
@@ -52,16 +212,19 @@ export interface User extends Node {
     projects: Project[];
 }
 
-export interface Backlog extends Node {
-    id: string;
-    project: Project;
-}
-
 export interface Board extends Node {
     id: string;
     name: string;
     description?: string;
     project: Project;
+    singleTasks: Task[];
+    taskGroups: TaskGroup[];
+    tasksOrder: BoardTasksOrderItem[];
+}
+
+export interface BoardTasksOrderItem {
+    taskId: string;
+    type: BoardTaskType;
 }
 
 export interface Project extends Node {
@@ -69,8 +232,46 @@ export interface Project extends Node {
     name: string;
     description?: string;
     color: Color;
-    backlog: Backlog;
     boards: Board[];
     members: User[];
     admin: User;
+}
+
+export interface Subtask extends Node {
+    id: string;
+    title: string;
+    description?: string;
+    isCompleted: boolean;
+    assign?: User;
+    task: Task;
+    workTime?: number;
+    scheduledTime?: number;
+}
+
+export interface Task extends Node {
+    id: string;
+    title: string;
+    description?: string;
+    status: Status;
+    subtasks: Subtask[];
+    assign?: User;
+    project: Project;
+    board: Board;
+    taskGroup?: TaskGroup;
+    workTime?: number;
+    scheduledTime?: number;
+    subtasksOrder?: string[];
+}
+
+export interface TaskGroup extends Node {
+    id: string;
+    title: string;
+    description?: string;
+    status: Status;
+    tasks: Task[];
+    assign?: User;
+    project: Project;
+    board: Board;
+    scheduledTime?: number;
+    tasksOrder: string[];
 }
