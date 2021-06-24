a07cc253180e34198024402f4a3cf6bb5856e939
shuufei <digital.shuufei@gmail.com>
2021-06-24 23:28:54 +0900

    ローカルでfrontnedからgraphql-api実行する際に、認証エラーが発生しないようにする

--- apps/frontend/desktop-app/src/app/graphql.module.ts
+++ apps/frontend/desktop-app/src/app/graphql.module.ts
@@ -1,12 +1,27 @@
+import { HttpHeaders } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
+import { COGNITO_AUTHENTICATION_PROVIDER } from '@bison/shared/constants';
 import { APOLLO_OPTIONS } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
+import { environment } from '../environments/environment';
 
+// TODO: environmen.productionでurlを変更する
 const uri = 'http://localhost:3333/graphql';
+const headers = new HttpHeaders(
+  environment.production
+    ? {}
+    : {
+        [COGNITO_AUTHENTICATION_PROVIDER]:
+          'cognito-idp.region.amazonaws.com/user_pool_id,cognito-idp.region.amazonaws.com/user_pool_id:CognitoSignIn:testUser0001',
+      }
+);
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<unknown> {
   return {
-    link: httpLink.create({ uri }),
+    link: httpLink.create({
+      uri,
+      headers,
+    }),
     cache: new InMemoryCache(),
   };
 }
