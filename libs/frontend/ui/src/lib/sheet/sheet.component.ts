2f9228d1e2126d2b92d052af42973218d5c1af63
shuufei <digital.shuufei@gmail.com>
2021-06-27 14:34:56 +0900

    sheetの表示非表示animationを修正

--- libs/frontend/ui/src/lib/sheet/sheet.component.ts
+++ libs/frontend/ui/src/lib/sheet/sheet.component.ts
@@ -1,3 +1,4 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import {
   ChangeDetectionStrategy,
   Component,
@@ -18,6 +19,31 @@ export type State = {
   styleUrls: ['./sheet.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [RxState],
+  animations: [
+    trigger('fadeInOutOverlay', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.1s 0s ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('0.25s 0s ease-out', style({ opacity: 0 })),
+      ]),
+    ]),
+    trigger('fadeInOutSheet', [
+      transition(':enter', [
+        style({ transform: 'translateY(25%)' }),
+        animate('0.2s 0s ease-out', style({ transform: 'translateY(0)' })),
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(0)', opacity: 1 }),
+        animate(
+          '0.2s 0s ease-out',
+          style({ transform: 'translateY(25%)', opacity: 0 })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class SheetComponent implements OnInit {
   @Input() triggerEl?: HTMLElement;
@@ -43,5 +69,8 @@ export class SheetComponent implements OnInit {
     );
     this.state.connect('isOpen', this.onClickOverlay$.pipe(mapTo(false)));
     this.state.connect('isOpen', this.onClickCloseButton$.pipe(mapTo(false)));
+    this.state.set({
+      isOpen: false,
+    });
   }
 }
