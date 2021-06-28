f3d42ee520f12ee71d1f945c7fcca3d96dc09798
shuufei <digital.shuufei@gmail.com>
2021-06-28 23:58:16 +0900

    app moduleにbrowserAnimationsModuleを追加

--- apps/frontend/desktop-app/src/app/app.module.ts
+++ apps/frontend/desktop-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GraphQLModule } from './graphql.module';
@@ -12,6 +13,7 @@ import { ProjectPageModule } from './pages/project-page/project-page.module';
     AppRoutingModule,
     ProjectPageModule,
     BrowserModule,
+    BrowserAnimationsModule,
     GraphQLModule,
     HttpClientModule,
   ],
