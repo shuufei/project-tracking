6d6049010570c46fea7019c7ec0a82b928f1d9f5
shuufei <digital.shuufei@gmail.com>
2021-07-23 15:44:45 +0900

    feat: 作業者変更ボタンコンポーネント実装

---
+++ apps/frontend/desktop-app/src/app/shared/components/task-dialog-assign-change-button/task-dialog-assign-change-button.component.ts
@@ -0,0 +1,62 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+} from '@angular/core';
+import { User } from '@bison/shared/domain';
+import { RxState } from '@rx-angular/state';
+import { combineLatest, Subject } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+type State = {
+  users: User[];
+  selectedUserId?: User['id'];
+};
+
+@Component({
+  selector: 'bis-task-dialog-assign-change-button',
+  templateUrl: './task-dialog-assign-change-button.component.html',
+  styleUrls: ['./task-dialog-assign-change-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [RxState],
+})
+export class TaskDialogAssignChangeButtonComponent implements OnInit {
+  @Input()
+  set users(value: User[]) {
+    this.state.set('users', () => value);
+  }
+  @Input()
+  set selectedUserId(value: User['id']) {
+    this.state.set('selectedUserId', () => value);
+  }
+  @Output() selectedUser = new EventEmitter<User['id']>();
+
+  /**
+   * State
+   */
+  readonly state$ = this.state.select();
+  readonly selectedUserImageSrc$ = combineLatest([
+    this.state.select('users'),
+    this.state.select('selectedUserId'),
+  ]).pipe(map(([users, id]) => users.find((v) => v.id === id)?.icon));
+
+  /**
+   * Event
+   */
+  readonly onChangedSelectedUserId$ = new Subject<State['selectedUserId']>();
+
+  constructor(private state: RxState<State>) {}
+
+  ngOnInit(): void {
+    this.state.set({
+      users: [],
+    });
+    this.state.connect('selectedUserId', this.onChangedSelectedUserId$);
+    this.state.hold(this.state.select('selectedUserId'), (userId) => {
+      this.selectedUser.emit(userId);
+    });
+  }
+}
