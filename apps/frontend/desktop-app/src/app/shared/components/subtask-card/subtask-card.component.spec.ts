38f61e9df9cda30da9cf8e96a89b54a5a6e23f6b
shuufei <digital.shuufei@gmail.com>
2021-08-10 18:35:56 +0900

    test: UTのエラーを修正. 一部削除

--- apps/frontend/desktop-app/src/app/shared/components/subtask-card/subtask-card.component.spec.ts
+++ apps/frontend/desktop-app/src/app/shared/components/subtask-card/subtask-card.component.spec.ts
@@ -1,15 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  ApolloDataQuery,
+  APOLLO_DATA_QUERY,
+} from '@bison/frontend/application';
+import { createSpyObj } from 'jest-createspyobj';
+import { of } from 'rxjs';
 import { SubtaskCardComponent } from './subtask-card.component';
 import { SubtaskCardModule } from './subtask-card.module';
 
 describe('SubtaskCardComponent', () => {
   let component: SubtaskCardComponent;
   let fixture: ComponentFixture<SubtaskCardComponent>;
+  const apolloDataQuery = createSpyObj(ApolloDataQuery);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [SubtaskCardModule],
+      providers: [
+        {
+          provide: APOLLO_DATA_QUERY,
+          useValue: apolloDataQuery,
+        },
+      ],
     }).compileComponents();
+  });
+
+  beforeEach(() => {
+    apolloDataQuery.queryUsers.mockReturnValue(
+      of({ data: { users: [] }, loading: false, networkStatus: 7 })
+    );
   });
 
   beforeEach(() => {
--- apps/frontend/desktop-app/src/app/shared/components/task-dialog/task-dialog-task-content/task-dialog-task-content.component.spec.ts
+++ apps/frontend/desktop-app/src/app/shared/components/task-dialog/task-dialog-task-content/task-dialog-task-content.component.spec.ts
@@ -1,16 +1,64 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {
+  ApolloDataQuery,
+  APOLLO_DATA_QUERY,
+  CreateSubtaskUsecase,
+  CREATE_SUBTASK_USECASE,
+  DeleteTaskUsecase,
+  DELETE_TASK_USECASE,
+  UpdateTaskUsecase,
+  UPDATE_TASK_USECASE,
+} from '@bison/frontend/application';
+import { createSpyObj } from 'jest-createspyobj';
+import { of } from 'rxjs';
+import { TaskDialogModule } from '../task-dialog.module';
+import { TaskDialogService } from '../task-dialog.service';
 import { TaskDialogTaskContentComponent } from './task-dialog-task-content.component';
 
 describe('TaskDialogTaskContentComponent', () => {
   let component: TaskDialogTaskContentComponent;
   let fixture: ComponentFixture<TaskDialogTaskContentComponent>;
+  const taskDialogService = createSpyObj(TaskDialogService);
+  const apolloDataQuery = createSpyObj(ApolloDataQuery);
+  const updateTaskUsecase = createSpyObj(UpdateTaskUsecase);
+  const deleteTaskUsecase = createSpyObj(DeleteTaskUsecase);
+  const createSubtaskUsecase = createSpyObj(CreateSubtaskUsecase);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TaskDialogTaskContentComponent ]
-    })
-    .compileComponents();
+      imports: [TaskDialogModule],
+      providers: [
+        {
+          provide: TaskDialogService,
+          useValue: taskDialogService,
+        },
+        {
+          provide: APOLLO_DATA_QUERY,
+          useValue: apolloDataQuery,
+        },
+        {
+          provide: UPDATE_TASK_USECASE,
+          useValue: updateTaskUsecase,
+        },
+        {
+          provide: DELETE_TASK_USECASE,
+          useValue: deleteTaskUsecase,
+        },
+        {
+          provide: CREATE_SUBTASK_USECASE,
+          useValue: createSubtaskUsecase,
+        },
+      ],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    apolloDataQuery.queryUsers.mockReturnValue(
+      of({ data: { users: [] }, loading: false, networkStatus: 7 })
+    );
+    apolloDataQuery.queryProject.mockReturnValue(
+      of({ data: { project: undefined }, loading: false, networkStatus: 7 })
+    );
   });
 
   beforeEach(() => {
