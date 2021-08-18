b7c890aa99b5a3b7c0f6e8111ef82ffc35158c23
shuufei <digital.shuufei@gmail.com>
2021-08-18 23:58:52 +0900

    fix: テスト用ページに作業レポートシートを開く実装を追加

--- apps/frontend/desktop-app/src/app/shared/components/report-sheet/report-sheet.component.ts
+++ apps/frontend/desktop-app/src/app/shared/components/report-sheet/report-sheet.component.ts
@@ -1,16 +1,26 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  Input,
+  OnInit,
+} from '@angular/core';
+import { RxState } from '@rx-angular/state';
+
+type State = Record<string, never>;
 
 @Component({
   selector: 'bis-report-sheet',
   templateUrl: './report-sheet.component.html',
   styleUrls: ['./report-sheet.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [RxState],
 })
 export class ReportSheetComponent implements OnInit {
+  @Input() triggerEl?: HTMLElement;
 
-  constructor() { }
+  constructor(private state: RxState<State>) {}
 
   ngOnInit(): void {
+    return;
   }
-
 }
--- apps/frontend/desktop-app/src/app/shared/components/report-sheet/report-sheet.module.ts
+++ apps/frontend/desktop-app/src/app/shared/components/report-sheet/report-sheet.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SheetModule } from '@bison/frontend/ui';
 import { ReportSheetComponent } from './report-sheet.component';
 
 @NgModule({
   declarations: [ReportSheetComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SheetModule],
   exports: [ReportSheetComponent],
 })
 export class ReportSheetModule {}
