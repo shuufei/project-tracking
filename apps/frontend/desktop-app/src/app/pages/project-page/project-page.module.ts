46bcf9754738557a6200352ba47eb98f4d9c47dd
shuufei <digital.shuufei@gmail.com>
2021-08-19 21:41:00 +0900

    fix: Inject漏れ修正

--- apps/frontend/desktop-app/src/app/pages/project-page/project-page.module.ts
+++ apps/frontend/desktop-app/src/app/pages/project-page/project-page.module.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import {
+  ApolloDataQuery,
+  APOLLO_DATA_QUERY,
+} from '@bison/frontend/application';
 import { ProjectPageRoutingModule } from './project-page-routing.module';
 import { ProjectPageComponent } from './project-page.component';
 
@@ -7,6 +11,11 @@ import { ProjectPageComponent } from './project-page.component';
   declarations: [ProjectPageComponent],
   imports: [CommonModule, ProjectPageRoutingModule],
   exports: [ProjectPageComponent],
-  providers: [],
+  providers: [
+    {
+      provide: APOLLO_DATA_QUERY,
+      useClass: ApolloDataQuery,
+    },
+  ],
 })
 export class ProjectPageModule {}
