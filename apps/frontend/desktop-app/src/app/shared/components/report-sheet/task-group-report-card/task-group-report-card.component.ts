fcc30e4cc2ded045461b58022da28779968103a3
shuufei <digital.shuufei@gmail.com>
2021-08-26 23:29:13 +0900

    feat: taskGroupのtasksをorder順に表示する

--- apps/frontend/desktop-app/src/app/shared/components/report-sheet/task-group-report-card/task-group-report-card.component.ts
+++ apps/frontend/desktop-app/src/app/shared/components/report-sheet/task-group-report-card/task-group-report-card.component.ts
@@ -4,8 +4,10 @@ import {
   Input,
   OnInit,
 } from '@angular/core';
-import { TaskGroup } from '@bison/frontend/domain';
+import { Task, TaskGroup } from '@bison/frontend/domain';
 import { RxState } from '@rx-angular/state';
+import { map } from 'rxjs/operators';
+import { nonNullable } from '../../../../util/custom-operators/non-nullable';
 
 type State = {
   taskGroup?: TaskGroup;
@@ -35,6 +37,18 @@ export class TaskGroupReportCardComponent implements OnInit {
    * State
    */
   readonly state$ = this.state.select();
+  readonly tasks$ = this.state.select('taskGroup').pipe(
+    nonNullable(),
+    map((taskGroup) => {
+      const orderItems = taskGroup.tasksOrder
+        .map((id) => taskGroup.tasks.find((task) => task.id === id))
+        .filter((v): v is Task => v != null);
+      const remainedTasks = taskGroup.tasks.filter(
+        (task) => !orderItems.find((v) => v.id === task.id)
+      );
+      return [...orderItems, ...remainedTasks];
+    })
+  );
 
   constructor(private state: RxState<State>) {}
 
