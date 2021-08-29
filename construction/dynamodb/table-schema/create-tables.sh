c9b3d199f62a798266f443708671650a88431eba
shuufei <digital.shuufei@gmail.com>
2021-08-30 02:32:01 +0900

    feat: dbのsetup処理をnpm scriptに追加

--- construction/dynamodb/table-schema/create-tables.sh
+++ construction/dynamodb/table-schema/create-tables.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -euxv
 
-aws dynamodb create-table --cli-input-json file://./Project_User.json --endpoint http://localhost:8000 > /dev/null
-aws dynamodb create-table --cli-input-json file://./Board.json --endpoint http://localhost:8000 > /dev/null
-aws dynamodb create-table --cli-input-json file://./TaskGroup.json --endpoint http://localhost:8000 > /dev/null
-aws dynamodb create-table --cli-input-json file://./Task.json --endpoint http://localhost:8000 > /dev/null
-aws dynamodb create-table --cli-input-json file://./Subtask.json --endpoint http://localhost:8000 > /dev/null
+readonly BASE_PATH=$(dirname $0)
+
+echo $BASE_PATH
+
+aws dynamodb create-table --cli-input-json file://${BASE_PATH}/Project_User.json --endpoint http://localhost:8000 > /dev/null
+aws dynamodb create-table --cli-input-json file://${BASE_PATH}/Board.json --endpoint http://localhost:8000 > /dev/null
+aws dynamodb create-table --cli-input-json file://${BASE_PATH}/TaskGroup.json --endpoint http://localhost:8000 > /dev/null
+aws dynamodb create-table --cli-input-json file://${BASE_PATH}/Task.json --endpoint http://localhost:8000 > /dev/null
+aws dynamodb create-table --cli-input-json file://${BASE_PATH}/Subtask.json --endpoint http://localhost:8000 > /dev/null
