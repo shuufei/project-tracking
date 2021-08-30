9c4df9362d5c614199a967e9068346807fcf584c
shuufei <digital.shuufei@gmail.com>
2021-08-30 21:34:27 +0900

    project detailに状態表示を移植

--- apps/frontend/desktop-app/src/app/pages/project-page/project-detail/project-detail.component.ts
+++ apps/frontend/desktop-app/src/app/pages/project-page/project-detail/project-detail.component.ts
@@ -1,16 +1,42 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { COLOR, Color } from '@bison/shared/domain';
+import { RxState } from '@rx-angular/state';
+
+type State = {
+  title: string;
+  color: Color;
+  description: string;
+};
 
 @Component({
   selector: 'bis-project-detail',
   templateUrl: './project-detail.component.html',
   styleUrls: ['./project-detail.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [RxState],
 })
-export class ProjectDetailComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProjectDetailComponent {
+  @Input()
+  set title(value: string) {
+    this.state.set('title', () => value);
+  }
+  @Input()
+  set color(value: Color) {
+    this.state.set('color', () => value);
+  }
+  @Input()
+  set description(value: string) {
+    this.state.set('description', () => value);
   }
 
+  // State
+  readonly state$ = this.state.select();
+
+  constructor(private state: RxState<State>) {
+    this.state.set({
+      title: '',
+      color: COLOR.Gray,
+      description: ''
+    });
+  }
 }
