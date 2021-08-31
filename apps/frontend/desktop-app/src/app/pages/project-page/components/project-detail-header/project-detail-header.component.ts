1f405af9a30059924c10c04808a2ff6c8c916960
shuufei <digital.shuufei@gmail.com>
2021-08-31 22:33:13 +0900

    プロジェクト詳細ヘッダーの雛形を実装

--- apps/frontend/desktop-app/src/app/pages/project-page/components/project-detail-header/project-detail-header.component.ts
+++ apps/frontend/desktop-app/src/app/pages/project-page/components/project-detail-header/project-detail-header.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { RxState } from '@rx-angular/state';
+import { Subject } from 'rxjs';
+
+type State = {
+};
 
 @Component({
   selector: 'bis-project-detail-header',
   templateUrl: './project-detail-header.component.html',
   styleUrls: ['./project-detail-header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [RxState],
 })
 export class ProjectDetailHeaderComponent implements OnInit {
 
-  constructor() { }
+  readonly state$ = this.state.select();
 
-  ngOnInit(): void {
+  private readonly onInit$ = new Subject();
+
+  constructor(private state: RxState<State>) {
+    this.state.set({
+    });
   }
 
+  ngOnInit(): void {
+    this.onInit$.next();
+  }
 }
