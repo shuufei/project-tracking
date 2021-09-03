1a6e52d6cd4246d00aafa0982a1801094ad23b2b
shuufei <digital.shuufei@gmail.com>
2021-09-03 23:03:33 +0900

    board取得処理追加、ボード詳細画面大まかに実装

--- apps/frontend/desktop-app/src/app/pages/project-page/project-page.module.ts
+++ apps/frontend/desktop-app/src/app/pages/project-page/project-page.module.ts
@@ -15,7 +15,6 @@ import {
   ProjectNavigationModule,
   SheetFooterModule,
   SheetModule,
-  TaskCardModule,
   UserIconModule,
 } from '@bison/frontend/ui';
 import { TuiNotificationsModule } from '@taiga-ui/core';
@@ -24,6 +23,7 @@ import { BoardDeleteDialogModule } from '../../shared/components/board-delete-di
 import { BoardUpdateSheetModule } from '../../shared/components/board-update-sheet/board-update-sheet.module';
 import { ProjectDeleteDialogModule } from '../../shared/components/project-delete-dialog/project-delete-dialog.module';
 import { ProjectPropertyEditFormModule } from '../../shared/components/project-property-edit-form/project-property-edit-form.module';
+import { TaskCardModule } from '../../shared/components/task-card/task-card.module';
 import { TaskDialogModule } from '../../shared/components/task-dialog/task-dialog.module';
 import { BoardDetailHeaderComponent } from './components/board-detail-header/board-detail-header.component';
 import { BoardDetailComponent } from './components/board-detail/board-detail.component';
