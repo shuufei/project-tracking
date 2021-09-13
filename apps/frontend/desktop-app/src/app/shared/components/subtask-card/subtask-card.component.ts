2cd383cb2e225a855aa2f84ca029af1b72590e21
shuufei <digital.shuufei@gmail.com>
2021-09-13 22:59:24 +0900

    fix: subtask cardのupdate処理時にlocalの状態を更新しない

--- apps/frontend/desktop-app/src/app/shared/components/subtask-card/subtask-card.component.ts
+++ apps/frontend/desktop-app/src/app/shared/components/subtask-card/subtask-card.component.ts
@@ -19,14 +19,12 @@ import {
   exhaustMap,
   filter,
   map,
-  pairwise,
-  startWith,
   switchMap,
   tap,
 } from 'rxjs/operators';
 import { convertToDomainSubtaskFromApiSubtask } from '../../../util/convert-to-domain-subtask-from-api-subtask';
+import { mapToUpdatedWorkTimeSecState } from '../../../util/custom-operators/map-to-updated-work-time-sec-state';
 import { nonNullable } from '../../../util/custom-operators/non-nullable';
-import { updateStateOnPause } from '../../../util/custom-operators/state-updators/update-state-on-pause';
 import { SubtaskFacadeService } from '../../facade/subtask-facade/subtask-facade.service';
 import {
   SUBTASK_FIELDS,
@@ -164,7 +162,7 @@ export class SubtaskCardComponent implements OnInit {
         filter((userId) => {
           return userId !== this.state.get('subtask')?.assignUser?.id;
         }),
-        exhaustMap((userId) => {
+        switchMap((userId) => {
           const subtask = this.state.get('subtask');
           if (subtask == null) return of(undefined);
           return this.subtaskFacade.updateAssignUser(userId, subtask);
@@ -177,48 +175,21 @@ export class SubtaskCardComponent implements OnInit {
         filter((isDone) => {
           return isDone !== this.state.get('subtask')?.isDone;
         }),
-        tap((isDone) => {
-          this.state.set('subtask', ({ subtask }) => {
-            return subtask == null
-              ? subtask
-              : {
-                  ...subtask,
-                  isDone,
-                };
-          });
-        }),
-        exhaustMap((isDone) => {
+        switchMap((isDone) => {
           const subtask = this.state.get('subtask');
           if (subtask == null) return of(undefined);
           return this.subtaskFacade.updateIsDoone(isDone, subtask);
         })
       )
     );
-    // TODO: taskと同じ処理になるので共通化する
     this.state.hold(
       this.onChangedWorkTimeSec$.pipe(
-        startWith(this.state.get('subtask')?.workTimeSec ?? 0),
-        pairwise(),
-        filter(([prev, sec]) => {
-          const diff = sec - prev;
-          const isChangedByCtrlBtn = diff > 1;
-          const isTracking =
-            this.state.get('subtask')?.workStartDateTimestamp != null;
-          return isChangedByCtrlBtn || !isTracking;
-        }),
-        map(([, current]) => current),
-        filter((sec) => {
-          return sec !== this.state.get('subtask')?.workTimeSec;
-        }),
-        exhaustMap((sec) => {
-          const subtask = this.state.get('subtask');
-          if (subtask == null) return of(undefined);
-          const workStartDateTimestamp =
-            subtask.workStartDateTimestamp && new Date().valueOf();
+        mapToUpdatedWorkTimeSecState(this.state, 'subtask'),
+        switchMap(({ updated, current }) => {
           return this.subtaskFacade.updateWorkTimeSec(
-            sec,
-            workStartDateTimestamp,
-            subtask
+            updated.workTimeSec,
+            updated.workStartDateTimestamp,
+            current
           );
         })
       )
@@ -228,18 +199,7 @@ export class SubtaskCardComponent implements OnInit {
         filter((sec) => {
           return sec !== this.state.get('subtask')?.scheduledTimeSec;
         }),
-        tap((sec) => {
-          this.state.set('subtask', (state) => {
-            const subtask = state.subtask;
-            return subtask == null
-              ? subtask
-              : {
-                  ...subtask,
-                  scheduledTimeSec: sec,
-                };
-          });
-        }),
-        exhaustMap((sec) => {
+        switchMap((sec) => {
           const subtask = this.state.get('subtask');
           if (subtask == null) return of(undefined);
           return this.subtaskFacade.updateScheduledTimeSec(sec, subtask);
@@ -248,28 +208,21 @@ export class SubtaskCardComponent implements OnInit {
     );
     this.state.hold(
       this.onClickedPlay$.pipe(
-        exhaustMap(() => {
+        switchMap(() => {
           const subtask = this.state.get('subtask');
           if (subtask == null) return of(undefined);
           const now = new Date();
-          this.state.set('subtask', (state) => {
-            const subtask = state.subtask;
-            return subtask == null
-              ? subtask
-              : {
-                  ...subtask,
-                  workStartDateTimestamp: now.valueOf(),
-                };
-          });
           return this.subtaskFacade.startTracking(now, subtask);
         })
       )
     );
     this.state.hold(
       this.onClickedPause$.pipe(
-        updateStateOnPause(this.state, 'subtask'),
-        exhaustMap(({ updated }) => {
-          return this.subtaskFacade.stopTracking(updated);
+        switchMap(() => {
+          const subtask = this.state.get('subtask');
+          if (subtask == null) return of(undefined);
+          const now = new Date();
+          return this.subtaskFacade.stopTracking(now, subtask);
         })
       )
     );
@@ -284,18 +237,10 @@ export class SubtaskCardComponent implements OnInit {
     );
     this.state.hold(
       this.onSubmitTitle$.pipe(
-        tap((title) => {
+        tap(() => {
           this.state.set('isEditableTitle', () => false);
-          this.state.set('subtask', ({ subtask }) => {
-            return subtask == null
-              ? subtask
-              : {
-                  ...subtask,
-                  title,
-                };
-          });
         }),
-        exhaustMap((title) => {
+        switchMap((title) => {
           const subtask = this.state.get('subtask');
           if (subtask == null) return of(undefined);
           return this.subtaskFacade.updateTitle(title, subtask);
